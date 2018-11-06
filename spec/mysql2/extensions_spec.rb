da5a621be395a3cfb240a4ea786201387b83beb9
TIT <sblohin@yandex.ru>
2018-11-06 13:46:23 +0300

    Update extensions_spec.rb

--- spec/mysql2/extensions_spec.rb
+++ spec/mysql2/extensions_spec.rb
@@ -1,23 +1,37 @@
 # frozen_string_literal: true
 
-RSpec.describe Mysql2::Extensions do
-  it 'has a version number' do
-    expect(Mysql2::Extensions::VERSION).not_to be nil
+describe Mysql2::Extensions do
+  context 'class' do
+    it 'has a version number' do
+      expect(Mysql2::Extensions::VERSION).not_to be_nil
+    end
   end
 
-  describe '#query' do
-    let(:client) { Mysql2::Client.new(username: 'root') }
+  context '#query' do
+    let :client do
+      Mysql2::Client.new username: 'root',
+                         password: ''
+    end
 
-    context 'with successful' do
-      it '#query' do
-        result = client.query('SELECT 42')
+    context 'when valid sql' do
+      it 'return array result' do
+        sql = <<~MYSQL
+          SELECT 42;
+        MYSQL
+        result = client.query sql
+
         expect(result).to be_a Array
       end
     end
 
-    context 'with error' do
-      it '#query' do
-        expect { client.query('foo') }.to raise_error(Mysql2::Error, /SQL: foo/)
+    context 'when invalid sql' do
+      it 'return exception with queried sql' do
+        sql = <<~MYSQL
+          SELECT foo;
+        MYSQL
+
+        result = expect { client.query sql }
+        result.to raise_error(Mysql2::Error, /SQL: #{sql}$/)
       end
     end
   end
